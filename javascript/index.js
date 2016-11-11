a1d17ae13ee88def62a90e5e5567644f130af1c2
Muhannes <haneda-3@student.ltu.se>
2016-11-11 10:40:26 +0100

    Added jQuery, bootstrap and fileStructure for js and css.

---
+++ javascript/index.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $("#javascripttest").html("Hello from javascript too!");
+})
--- python/about.py
+++ python/about.py
@@ -7,11 +7,8 @@ from python import JINJA_ENVIRONMENT
 class About(webapp2.RequestHandler):
     def get(self):
 
-        template_values = {
-
-        }
         template = JINJA_ENVIRONMENT.get_template('about.html')
-        self.response.write(template.render(template_values))
+        self.response.write(template.render())
 
 # [START app]
 app = webapp2.WSGIApplication([

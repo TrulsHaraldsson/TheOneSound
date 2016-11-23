11a030075ed62cc4d5cd93457d0adf1ff7070467
Muhannes <haneda-3@student.ltu.se>
2016-11-23 14:12:45 +0100

    fixed entityparser

--- python/util/entityparser.py
+++ python/util/entityparser.py
@@ -1,11 +1,18 @@
+from python.databaseKinds import Rating
+from python.databaseKinds import Description
+from python.databaseKinds import Comment
+from python.databaseKinds import Band
+from google.appengine.ext import ndb
+
 def entities_to_dic_list(entities):
     dic_list = []
-    for entity in entites:
-        dic_with_id =entity_to_dic(entity)
+    print("in dic_list function")
+    for entity in entities:
+        dic_with_id = entity_to_dic(entity)
         dic_list.append(dic_with_id)
     return dic_list
 
 def entity_to_dic(entity):
-    dic = entity.to_dic()
+    dic = entity.to_dict()
     dic["id"] = entity.key.id()
     return dic

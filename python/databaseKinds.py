ea564ce816eb450837660aa1f312adc767214230
Truls Haraldsson <truhar-3@student.ltu.se>
2016-11-30 21:52:37 +0100

    Fixed PEP 8 code style

--- python/databaseKinds.py
+++ python/databaseKinds.py
@@ -1,18 +1,22 @@
 from google.appengine.ext import ndb
 
+
 class Rating(ndb.Model):
     likes = ndb.IntegerProperty()
     dislikes = ndb.IntegerProperty()
 
+
 class Description(ndb.Model):
     description = ndb.StringProperty()
-    #more to be added, pics and such.
+    # more to be added, pics and such.
 
-#Has user as parent
+
+# Has user as parent
 class Comment(ndb.Model):
     content = ndb.StringProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
     rating = ndb.StructuredProperty(Rating)
+
 
 class Band(ndb.Model):
     name = ndb.StringProperty()
@@ -21,18 +25,20 @@ class Band(ndb.Model):
     description = ndb.StructuredProperty(Description)
 
 
-#Has Band as parent
+# Has Band as parent
 class Album(ndb.Model):
     name = ndb.StringProperty()
     comment = ndb.StructuredProperty(Comment, repeated=True)
     rating = ndb.StructuredProperty(Rating)
     description = ndb.StructuredProperty(Description)
 
-#Has Album as parent
+
+# Has Album as parent
 class Track(ndb.Model):
     name = ndb.StringProperty()
     comment = ndb.StructuredProperty(Comment, repeated=True)
     rating = ndb.StructuredProperty(Rating)
+
 
 class Account(ndb.Model):
     name = ndb.StringProperty()

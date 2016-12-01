7f6fe66c08394549e731a8d991dd64537757a717
Muhannes <haneda-3@student.ltu.se>
2016-12-01 14:25:44 +0100

    fixed main page and deleted testpage

---
+++ python/frontend/main.py
@@ -0,0 +1,21 @@
+import webapp2
+from python.frontend import JINJA_ENVIRONMENT
+from python.util import loginhelper
+
+
+# [START main_page]
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        template_values = {}
+        loginhelper.add_login_values(template_values, self)
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+# [END main_page]
+
+
+# [START app]
+app = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=True)
+# [END app]

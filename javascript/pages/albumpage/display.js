b6b8ae640cced995624a856fdcd8c263c8efc1d8
Truls Haraldsson <truhar-3@student.ltu.se>
2016-12-12 15:45:36 +0100

    Merge

--- javascript/pages/albumpage/display.js
+++ javascript/pages/albumpage/display.js
@@ -2,5 +2,10 @@ $(document).ready(function() {
 
   addSubmitListener("#update-picture-form", "albums");
   addSubmitListener("#update-description-form", "albums");
+  addPostSubmitListener("#new-entity-form", "tracks")
 
+
+  $("#button-add-entity").click(function(){
+    $("#div-add-entity").toggle();
+  });
 })
